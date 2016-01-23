3f9b7d1ef05be2f74de20574ffafb3565a6b3935
Andrea Giacomo Baldan <a.g.baldan@gmail.com>
2016-01-23 19:42:16 +0100

    Added some classes

---
+++ src/main/java/it/unipd/math/pcd/actors/impl/LocalActorRef.java
@@ -0,0 +1,20 @@
+package it.unipd.math.pcd.actors.impl;
+
+import it.unipd.math.pcd.actors.ActorSystem;
+import it.unipd.math.pcd.actors.Message;
+import it.unipd.math.pcd.actors.ActorRef;
+
+/**
+ * Created by codep on 1/23/16.
+ */
+public class LocalActorRef<T extends  Message> extends AbsActorRef<T> {
+
+    public LocalActorRef(ActorSystem system) {
+        super(system);
+    }
+
+    @Override
+    public void send(T message, ActorRef to) {
+
+    }
+}
